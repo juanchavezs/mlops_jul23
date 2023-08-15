2f8ca64d9ba5e8c9f8c2ae3ad536d8792f8b852b
Juan Pablo Chavez <juan.chavezs@coppel.com>
2023-08-14 23:10:36 -0700

    API 2

--- proyecto final/test/test_itesm_mlops.py
+++ proyecto final/test/test_itesm_mlops.py
@@ -1,7 +1,19 @@
 import pytest
-import DataPreprocessor()
 import pandas as pd
 import os
+import sys
+
+import os
+import sys
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(project_root)
+
+print("Actual Directory:", current_dir)
+
+from itesm_mlops.preprocess.preprocess_data import DataPreprocessor
+
 
 def test_data_existence():
     """
