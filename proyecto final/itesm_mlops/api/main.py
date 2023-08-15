c09955b3286c1f1350449189ddd31b35c9c17d58
Juan Pablo Chavez <juan.chavezs@coppel.com>
2023-08-14 23:45:55 -0700

    final

--- proyecto final/itesm_mlops/api/main.py
+++ proyecto final/itesm_mlops/api/main.py
@@ -9,30 +9,48 @@ sys.path.append(parent_dir)
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
 
-from predictor.predict import ModelPredictor
-from .models.models import Titanic
+from predictor.predict import LoadAndPredict
+from .models.models import data_market
 
 app = FastAPI()
 
 @app.get('/', status_code=200)
 async def healthcheck():
-    return 'Titanic classifier is all ready to go!'
+    return 'Cluster Classifiers is ready!'
 
 @app.post('/predict')
-def extract_name(titanic_features: Titanic):
-    predictor = ModelPredictor("/Users/Usuario 1/Desktop/repos/itesm-mlops/module-3/session-11/itesm_mlops/itesm_mlops/models/logistic_regression_output.pkl")
-    X = [titanic_features.pclass_nan,
-        titanic_features.age_nan,
-        titanic_features.sibsp_nan,
-        titanic_features.parch_nan,
-        titanic_features.fare_nan,
-        titanic_features.sex_male,
-        titanic_features.cabin_Missing,
-        titanic_features.cabin_rare,
-        titanic_features.embarked_Q,
-        titanic_features.embarked_S,
-        titanic_features.title_Mr,
-        titanic_features.title_Mrs,
-        titanic_features.title_rar]
+def extract_name(market_features: data_market):
+    predictor = predict_clusters("/Users/Usuario 1/Desktop/repos/mlops_jul23/proyecto final/itesm_mlops/models/kmeans_4_clusters_model.pkl")
+    X = [market_features.ID,
+    market_features.Educationduation,
+    market_features.Year_Birth,
+    market_features.Marital_Status,
+    market_features.Income,
+    market_features.Kidhome,
+    market_features.Teenhome,
+    market_features.Dt_Customer, 
+    market_features.Recency,
+    market_features.MntWines,
+    market_features.MntFruits,
+    market_features.MntMeatProducts,
+    market_features.MntFishProducts,
+    market_features.MntSweetProducts,
+    market_features.MntGoldProds,
+    market_features.NumDealsPurchases,
+    market_features.NumWebPurchases,
+    market_features.NumCatalogPurchases,
+    market_features.NumStorePurchases,
+    market_features.NumWebVisitsMonth,
+    market_features.AcceptedCmp3,
+    market_features.AcceptedCmp4,
+    market_features.AcceptedCmp5,
+    market_features.AcceptedCmp1,
+    market_features.AcceptedCmp2,
+    market_features.Complain,
+    market_features.Z_CostContact, 
+    market_features.Z_Revenue,
+    market_features.Response]
     prediction = predictor.predict([X])
     return JSONResponse(f"Resultado predicción: {prediction}")
+
+
--- proyecto final/itesm_mlops/api/models/models.py
+++ proyecto final/itesm_mlops/api/models/models.py
@@ -1,38 +1,71 @@
 from pydantic import BaseModel
 
-
-class Titanic(BaseModel):
+class data_market(BaseModel):
     """
-    Represents a passenger on the Titanic with various attributes.
-    
-    Attributes:
-        pclass_nan (float): Placeholder for missing values in 'pclass' attribute.
-        age_nan (float): Placeholder for missing values in 'age' attribute.
-        sibsp_nan (float): Placeholder for missing values in 'sibsp' attribute.
-        parch_nan (float): Placeholder for missing values in 'parch' attribute.
-        fare_nan (float): Placeholder for missing values in 'fare' attribute.
-        sex_male (float): Placeholder for 'sex' attribute being male.
-        cabin_Missing (float): Placeholder for 'cabin' attribute being missing.
-        cabin_rare (float): Placeholder for 'cabin' attribute being rare.
-        embarked_Q (float): Placeholder for 'embarked' attribute being Queenstown.
-        embarked_S (float): Placeholder for 'embarked' attribute being Southampton.
-        title_Mr (float): Placeholder for 'title' attribute being Mr.
-        title_Mrs (float): Placeholder for 'title' attribute being Mrs.
-        title_rar (float): Placeholder for 'title' attribute being rare.
-    """
+    - ID: Customer's unique identifier
+    - Year_Birth: Customer's birth year
+    - Education: Customer's education level
+    - Marital_Status: Customer's marital status
+    - Income: Customer's yearly household income
+    - Kidhome: Number of children in customer's household
+    - Teenhome: Number of teenagers in customer's household
+    - Dt_Customer: Date of customer's enrollment with the company
+    - Recency: Number of days since customer's last purchase
+    - Complain: 1 if the customer complained in the last 2 years, 0 otherwise
 
-    pclass_nan: float
-    age_nan: float
-    sibsp_nan: float
-    parch_nan: float
-    fare_nan: float
-    sex_male: float
-    cabin_Missing: float
-    cabin_rare: float
-    embarked_Q: float
-    embarked_S: float
-    title_Mr: float
-    title_Mrs: float
-    title_rar: float
+##### **Products**
+
+    - MntWines: Amount spent on wine in last 2 years
+    - MntFruits: Amount spent on fruits in last 2 years
+    - MntMeatProducts: Amount spent on meat in last 2 years
+    - MntFishProducts: Amount spent on fish in last 2 years
+    - MntSweetProducts: Amount spent on sweets in last 2 years
+    - MntGoldProds: Amount spent on gold in last 2 years
+
+##### **Promotion**
+
+    -  NumDealsPurchases: Number of purchases made with a discount
+    - AcceptedCmp1: 1 if customer accepted the offer in the 1st campaign, 0 otherwise
+    - AcceptedCmp2: 1 if customer accepted the offer in the 2nd campaign, 0 otherwise
+    - AcceptedCmp3: 1 if customer accepted the offer in the 3rd campaign, 0 otherwise
+    - AcceptedCmp4: 1 if customer accepted the offer in the 4th campaign, 0 otherwise
+    - AcceptedCmp5: 1 if customer accepted the offer in the 5th campaign, 0 otherwise
+    Response: 1 if customer accepted the offer in the last campaign, 0 otherwise
+
+##### **Place**
+
+    - NumWebPurchases: Number of purchases made through the company’s website
+    - NumCatalogPurchases: Number of purchases made using a catalogue
+    - NumStorePurchases: Number of purchases made directly in stores
+    - NumWebVisitsMonth: Number of visits to company’s website in the last month    """
     
+    ID: int  = 1
+    Education: str = 'Graduation'
+    Year_Birth:  int = 1957
+    Marital_Status: str = 'Single'
+    Income: float
+    Kidhome: int  
+    Teenhome: int  
+    Dt_Customer: str = '04-09-2012' 
+    Recency: float  = 50
+    MntWines: float  = 635
+    MntFruits: float  = 88
+    MntMeatProducts: float =  546
+    MntFishProducts: float  = 172
+    MntSweetProducts: float  = 88
+    MntGoldProds: float  =88
+    NumDealsPurchases: float = 3
+    NumWebPurchases: float  = 8
+    NumCatalogPurchases: float = 10
+    NumStorePurchases: float  = 4
+    NumWebVisitsMonth: float  = 7
+    AcceptedCmp3: float  = 0
+    AcceptedCmp4: float  = 0
+    AcceptedCmp5: float  = 0
+    AcceptedCmp1: float  = 0
+    AcceptedCmp2: float  = 0
+    Complain: float  = 0
+    Z_CostContact: float = 3 
+    Z_Revenue: float  = 11
+    Response: float = 1
     
